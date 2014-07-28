3e3173cc40f3536de75f89948ca240e5200f93f5
Joe Love <joe@lemondigits.com>
2014-07-28 01:17:32 +0100

    Initial commit

---
+++ public/scripts/main.js
@@ -0,0 +1,16 @@
+(function() {
+  var toggle_classes = 'experience__toggle--more experience__toggle--less',
+      $experience = $('.experience__more-info'),
+      $experience_toggle = $('.experience__toggle');
+
+  $experience.hide();
+  $experience_toggle.css('display', 'inline-block');
+  $experience_toggle.on('click', function() {
+    $(this).toggleClass(toggle_classes);
+    $experience.toggle();
+  });
+
+  if (jQuery.browser.mobile) {
+    $('.contact-details__phone').show();
+  }
+}());
