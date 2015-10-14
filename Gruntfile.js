15bb2a1b47bf203a0bf98b1d9a962249e10cbffa
Joe Love <git@joelove.uk>
2015-10-14 11:47:41 +0100

    added grunt watch and concat css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,48 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        less: {
-            development: {
-                options: {
-                    paths: ["public"]
-                },
-                files: { "public/style.css": "assets/less/main.less" }
-            },
-            production: {
-                options: {
-                    paths: ["public"],
-                    cleancss: true
-                },
-                files: { "public/style.css": "assets/less/main.less" }
-            }
-        }
-    });
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.registerTask('default', ['less']);
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          paths: ["public"]
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['assets/less/*.less'],
+            dest: 'assets/css/',
+            ext: '.css'
+          }
+        ]
+      },
+      production: {
+        options: {
+          paths: ["public"],
+          cleancss: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['assets/less/*.less'],
+            dest: 'assets/css/',
+            ext: '.css'
+          }
+        ]
+      },
+      concat_css: {
+        options: {},
+        all: {
+          src: ["/assets/css/*.css"],
+          dest: "public/style.css"
+        },
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concat-css');
+
+  grunt.registerTask('default', ['less']);
 };
