117a28cb04842102e396770e261561a16dbc94ee
Joe Love <git@joelove.uk>
2015-10-14 11:55:35 +0100

    simplified css concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,47 +2,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     less: {
       development: {
-        options: {
-          paths: ["public"]
-        },
-        files: [
-          {
-            expand: true,
-            cwd: 'src',
-            src: ['assets/less/*.less'],
-            dest: 'assets/css/',
-            ext: '.css'
-          }
-        ]
-      },
-      production: {
-        options: {
-          paths: ["public"],
-          cleancss: true
-        },
-        files: [
-          {
-            expand: true,
-            cwd: 'src',
-            src: ['assets/less/*.less'],
-            dest: 'assets/css/',
-            ext: '.css'
-          }
-        ]
-      },
-      concat_css: {
-        options: {},
-        all: {
-          src: ["/assets/css/*.css"],
-          dest: "public/style.css"
-        },
+        files: { "public/style.css": "assets/less/*.less" }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-concat-css');
 
   grunt.registerTask('default', ['less']);
 };
