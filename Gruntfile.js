ae0a4bd739ffe8519306704c2c85e22450e81082
Joe Love <git@joelove.uk>
2015-10-14 13:35:18 +0100

    added pdf builder and combined less output

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,35 @@
 module.exports = function(grunt) {
+  var wkhtmltopdf = require('wkhtmltopdf');
+  var fs = require('fs');
+
   grunt.initConfig({
     less: {
       development: {
-        files: { "public/style.css": "assets/less/*.less" }
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: { 'public/style.css': 'assets/less/*.less' }
       }
     },
     watch: {
       files: ['assets/less/*.less'],
       tasks: ['less']
-    }
+    },
+    wkhtmltopdf: {
+      dev: {
+        src: 'public/index.html',
+        dest: 'public/'
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-wkhtmltopdf');
 
   grunt.registerTask('default', ['less']);
+  grunt.registerTask('pdf', ['wkhtmltopdf']);
+  grunt.registerTask('all', ['less', 'wkhtmltopdf']);
 };
